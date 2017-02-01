2aa5e8e50bef631baa23c61901e60e52d00ea55a
Facundo Crusta <facundocrusta@gmail.com>
2017-02-01 11:49:33 -0300

    cambios menores

---
+++ bootcamp/src/main/java/com/bootcamp/java/proxy/proxy.java
@@ -0,0 +1,17 @@
+package com.bootcamp.java.proxy;
+
+import com.bootcamp.java.service.CountryWService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by facun on 01/02/2017.
+ */
+public class proxy {
+    @Autowired
+    CountryWService countryWService;
+
+
+    public proxy(){}
+
+
+}
